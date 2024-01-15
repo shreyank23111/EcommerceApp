2d0e72dc3dfc024eb84bdf069f6775455ecf1e6c
Shreyank Desai <shreyankdessai6@gmail.com>
2024-01-15 22:48:12 +0530

    started with brand controller

---
+++ Backend/Src/Controllers/brand.controller.js
@@ -0,0 +1,7 @@
+import { Category } from "../Models/Category.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const brand = (async(req, rse) =>{
+
+});
